ccc7a96f4aa45c22ef873d3977aa33acaedb8ffc
Melissa Christman <melchrstmn@gmail.com>
2019-11-07 23:06:13 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,9 +2,16 @@ class Dog
   attr_accessor :owner, :mood
   attr_reader :name
   
+  @@all = []
+  
   def initialize(name, owner)
     @name = name
     @owner = owner
     @mood = "nervous"
+    @@all << self
+  end
+  
+  def self.all
+    @@all
   end
 end
